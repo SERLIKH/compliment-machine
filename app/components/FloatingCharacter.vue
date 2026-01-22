d6e40cb0dacab4fc2daa63c6e3f895e5f2c11045
SERLIKH <serli.khairunisa@gmail.com>
2026-01-22 15:23:21 +0700

    Initial commit - The Compliment Machine

---
+++ app/components/FloatingCharacter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="absolute group transition-transform duration-300 hover:scale-105" :style="style">
+        <img :src="ori" class="absolute inset-0 transition-opacity duration-300 group-hover:opacity-0" />
+        <img :src="hover" class="absolute inset-0 opacity-0 transition-opacity duration-300 group-hover:opacity-100" />
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    ori: String,
+    hover: String,
+    style: Object
+})
+</script>
